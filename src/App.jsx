f1228bf9b3e5f6e303d469a0e6c622a0e23c85e2
Hossain394 <hossain394nu@gamil.com>
2025-10-08 15:34:14 +0600

    something

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import { useState } from "react"
 
 
-function App() {
-  let [ami, SetAmi] = useState(0);
- let handleIncrement = ()=>{
-   SetAmi(ami +1)
-  }
-  
-  let handleDecrement = ()=>{
-    if (ami > 0) {
-      
-      SetAmi(ami -1)
-    }
-  }  
 
+
+function App() {
+ let [show, SetShow] = useState(false);
+ let handleClickOne = () =>{
+ SetShow(!show)
+  
+ };
   return (
     <>
-    <button onClick={handleDecrement}>-</button>
-    <h1>{ami}</h1>
-    <button onClick={handleIncrement}>+</button>
+    <button className="click" onClick={handleClickOne}>click</button>
+      {show == true && <div className="one"></div>}
+
     </>
   )
 }
 
 export default App
+// let [ami, SetAmi] = useState(0);
+//  let handleIncrement = ()=>{
+//    SetAmi(ami +1)
+//   }
+  
+//   let handleDecrement = ()=>{
+//     if (ami > 0) {
+      
+//       SetAmi(ami -1)
+//     }
+//   }  
